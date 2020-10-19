1bea420edb415f4a7ba73c4b5ad21098d6d700d9
Aayushi Gupta <60402246+aayushi0209@users.noreply.github.com>
2020-10-19 22:10:58 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ahujaalisha8@gmail.com'
-EMAIL_HOST_PASSWORD='alisha@0209'
+EMAIL_HOST_USER = '*****@gmail.com'
+EMAIL_HOST_PASSWORD='******'
